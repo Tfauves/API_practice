a7c00016ffbfe70dea1159c8a1e2f6a03e428964
Tfauves <68396275+Tfauves@users.noreply.github.com>
2021-06-21 10:14:24 -0500

    01meme generator

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const mainDiv = document.createElement("maindiv")
+const generateBtn = document.createElement("button")
+
+mainDiv.id = "maindiv"
+generateBtn.id = "gbtn"
+
+
+document.body.appendChild(mainDiv)
+mainDiv.appendChild(generateBtn)
